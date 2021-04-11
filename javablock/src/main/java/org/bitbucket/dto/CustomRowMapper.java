3c60f27fb5203bd392e81a64d06c557d706134b9
denisfedorovich <fedororvich.d.d@gmail.com>
2021-04-11 12:28:40 +0300

    First one

---
+++ javablock/src/main/java/org/bitbucket/dto/CustomRowMapper.java
@@ -0,0 +1,13 @@
+package org.bitbucket.dto;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * This interface use for map single entity for my sql database.
+ *
+ * @param <T> your entity
+ */
+public interface CustomRowMapper<T> {
+    T rowMap(ResultSet rs) throws SQLException;
+}
--- javablock/src/main/java/org/bitbucket/utils/JsonHelper.java
+++ javablock/src/main/java/org/bitbucket/utils/JsonHelper.java
@@ -2,4 +2,8 @@ package org.bitbucket.utils;
 
 public class JsonHelper {
 
+//    public static <T> String toFormat(T object){
+//
+//    }
+
 }
