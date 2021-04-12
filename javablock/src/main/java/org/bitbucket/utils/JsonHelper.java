c9f2109215fb6826a85273cf654fc069c06486ee
Boyko Nikolay <enzeox@gmail.com>
2021-04-12 14:07:17 +0300

    Логгер

--- javablock/src/main/java/org/bitbucket/utils/JsonHelper.java
+++ javablock/src/main/java/org/bitbucket/utils/JsonHelper.java
@@ -2,6 +2,8 @@ package org.bitbucket.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 import java.util.Optional;
 
@@ -9,12 +11,15 @@ public class JsonHelper {
 
     private static ObjectMapper mapper = new ObjectMapper();
 
+    private static final Logger logger = LogManager.getLogger("JsonHelper");
+
     public static <T> Optional<String> toFormat(T object) {
         Optional<String> result = Optional.empty();
         try {
             result = Optional.of(mapper.writeValueAsString(object));
         } catch (JsonProcessingException e) {
             //TODO - add logger
+            logger.error(e);
             e.printStackTrace();
         }
         return result;
@@ -23,5 +28,4 @@ public class JsonHelper {
     public static <T> Optional<T> fromFormat(String string, Class<T> cls){
         return Optional.of(mapper.convertValue(string,cls));
     }
-
 }
