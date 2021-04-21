69ecb57391bb4686cac98b757b48e6297917750b
agb1a <agb1ad33@gmail.com>
2021-04-22 01:15:09 +0300

    sasaa

--- javablock/src/main/java/org/bitbucket/config/DatabaseConfig.java
+++ javablock/src/main/java/org/bitbucket/config/DatabaseConfig.java
@@ -13,9 +13,9 @@ public class DatabaseConfig {
     public static DataSource getHikariDS(){
         HikariConfig config = new HikariConfig();
         config.setDriverClassName("org.postgresql.Driver");
-        config.setJdbcUrl("jdbc:postgresql://ec2-34-252-251-16.eu-west-1.compute.amazonaws.com/d2kkikhuddo0t4");
-        config.setUsername("piyjovprplzkvn");
-        config.setPassword("77da983c208285d6521db10052fdf957d81a31bf682eda96363a8fdcf9bebab6");
+        config.setJdbcUrl("jdbc:postgresql://ec2-34-252-251-16.eu-west-1.compute.amazonaws.com/de28o4hdufj7m1");
+        config.setUsername("qgvrweumbrinxe");
+        config.setPassword("89c37abe70b0c0a5b337f1a55edccb6e7064a1c93eaac14db2a4d747c42e2be5");
         config.addDataSourceProperty("cachePrepStmts", "true");
         config.addDataSourceProperty("prepStmtCacheSize", "250");
         config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
